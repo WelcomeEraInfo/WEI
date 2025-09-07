22f9d000c9f69316688dd4ec24f5057b6866952b
HUANGWEI <123373476+WelcomeEraInfo@users.noreply.github.com>
2025-09-07 22:54:26 +0800

    Update BMI.java
    规范else和Scanner

--- BMI.java
+++ BMI.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 
 public class BMI {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         System.out.print("请输入体重 | Please enter your weight (单位千克 | Unit kg):");
+        Scanner scanner = new Scanner(System.in);
         double kg = scanner.nextDouble();
         System.out.print("请输入身高 | Please enter your height (单位厘米 | Unit cm):");
         double cm = scanner.nextDouble();
@@ -13,8 +13,7 @@ public class BMI {
         System.out.println("正常值范围 | Normal value range:18.5～24.5");
         if (tizhi <= 24.5 && tizhi >= 18.5) {
             System.out.println("判断为正常 | Judged as normal");
-        }
-        else {
+        } else {
             System.out.println("判断为不正常 | Judged as abnormal");
         }
     }
