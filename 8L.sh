cdb074f6db2849bfa0eb56f69f9e3ab453bca973
HUANGWEI <123373476+WelcomeEraInfo@users.noreply.github.com>
2026-05-09 20:01:33 +0800

    Update and rename o.sh to 8L.sh

--- 8L.sh
+++ 8L.sh
@@ -7,19 +7,20 @@ for i in {1..10}; do
     done
 done
 printf "\b 启动！\n"
+sleep 0.1
 for ((i=0; i<=100; i+=1)); do
-    clear_line
-    echo -ne "提取：$i%"
+    printf "提取：$i%\n"
     sleep 0.1
 done
-printf "\n"
-sleep 0.5
+sleep 0.2
+clear
+sleep 0.1
 printf "构建"
-sleep 0.1
+sleep 0.2
 printf "."
-sleep 0.1
+sleep 0.2
 printf "."
-sleep 0.1
+sleep 0.2
 printf "."
 sleep 0.1
 clear
