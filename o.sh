83c3bf4ad994bdcf71694b135acfc44afe055bae
HUANGWEI <123373476+WelcomeEraInfo@users.noreply.github.com>
2026-05-09 19:36:07 +0800

    Update o.sh

--- o.sh
+++ o.sh
@@ -6,12 +6,23 @@ for i in {1..10}; do
         sleep 0.1
     done
 done
-printf "\b 获取"
+printf "\b 启动！\n"
+for ((i=0; i<=100; i+=1)); do
+    clear_line
+    echo -ne "提取：$i%"
+    sleep 0.1
+done
+printf "\n"
+sleep 0.5
+printf "构建"
 sleep 0.1
-printf "\b\b整理"
+printf "."
 sleep 0.1
-printf "\b\b启动！\n"
+printf "."
 sleep 0.1
+printf "."
+sleep 0.1
+clear
 printf "#include <reg51.h>\n"
 sleep 0.1
 printf "sbit key = P3^0;\n"
